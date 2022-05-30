4c5f657cdbc367b9f7548a66ef85f94bc26c5345
Siddharth <sidd.reddy1607@gmail.com>
2022-05-30 20:54:22 +0530

    Added Formula on Area Page and ensured gives input

--- area.js
+++ area.js
@@ -6,6 +6,13 @@ calculateButton.addEventListener("click",calculateArea);
 
 function calculateArea()
 {
+    if(inputs[0].value && inputs[1].value)
+    {
     var area=0.5*Number(inputs[0].value)*Number(inputs[1].value);
     output.innerText="The area of the triangle is "+area+" cm²";
+    }
+    else
+    {
+        output.innerText="Please enter both the fields"
+    }
 }
